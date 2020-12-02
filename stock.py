d12b3f27b687962977573ff0518d7d4422b5d7fe
Taptaplit <74630038+Taptaplit@users.noreply.github.com>
2020-12-02 18:20:05 -0500

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,15 +1,4 @@
-import tkinter as tk
-import requests
-from bs4 import BeautifulSoup
 
-
-def StockTracker(entry):
-    url = 'https://finance.yahoo.com/quote/' + str(entry)
-
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    price = soup.find_all("div", {"class":"My(6px) Pos(r) smartphone_Mt(6px)"})[0].find('span').text
-    label['text'] = str(price)
     
 
     
