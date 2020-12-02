9f7d5599811a48b3808b3b62e77866d8eb523b6a
Taptaplit <74630038+Taptaplit@users.noreply.github.com>
2020-12-02 18:20:50 -0500

    Update and rename frontend.py to Stock.py

--- Stock.py
+++ Stock.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 import requests
 from bs4 import BeautifulSoup
-from Connecter import button
+
+
+def StockTracker(entry):
+    url = 'https://finance.yahoo.com/quote/' + str(entry)
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    price = soup.find_all("div", {"class":"My(6px) Pos(r) smartphone_Mt(6px)"})[0].find('span').text
+    label['text'] = str(price)
 
 root = tk.Tk()
 
