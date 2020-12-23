01d16270984edf0ec7edee42c92afb1b9bece9ab
Taptaplit <74630038+Taptaplit@users.noreply.github.com>
2020-12-23 16:51:09 -0500

    Update Stock.py

--- Stock.py
+++ Stock.py
@@ -2,7 +2,6 @@ import tkinter as tk
 import requests
 from bs4 import BeautifulSoup
 
-
 def StockTracker(entry):
     url = 'https://finance.yahoo.com/quote/' + str(entry)
 
@@ -20,7 +19,7 @@ canvas.pack()
 frame = tk.Frame(root, bg="red", bd=5)
 frame.place(relwidth=0.85, relheight=0.1, relx="0.5", rely="0.1", anchor="n")
 
-
+button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
 button.place(relwidth=0.30, relx=0.7, relheight=1)
 
 entry = tk.Entry(frame, bg="purple", font=50)
