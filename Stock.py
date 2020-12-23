f53d748cf5f3eabd32c84895675c6a8dc325811d
Taptaplit <74630038+Taptaplit@users.noreply.github.com>
2020-12-23 16:52:44 -0500

    Update Stock.py

--- Stock.py
+++ Stock.py
@@ -19,7 +19,7 @@ canvas.pack()
 frame = tk.Frame(root, bg="red", bd=5)
 frame.place(relwidth=0.85, relheight=0.1, relx="0.5", rely="0.1", anchor="n")
 
-button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
+button = tk.Button(frame, text="Get Stock", font=40, command=lambda: StockTracker(entry.get()))
 button.place(relwidth=0.30, relx=0.7, relheight=1)
 
 entry = tk.Entry(frame, bg="purple", font=50)
